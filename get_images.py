0f84f6931a885301ea9b76834a7136b733fd9b78
Luke Griffiths <luke.griffiths@ngi.no>
2022-02-12 23:17:48 +0100

    Updated get images to get all images in area

--- get_images.py
+++ get_images.py
@@ -46,20 +46,35 @@ def get_image(coords,topath, zoom=18, verbose=False):
 
 if __name__ =='__main__':
 
-    # initial parameters
-    coords_start = (59.93717029103388, 10.807871254728301) # lat long coordinates of start point
-    num_lat = 15 # number of images to take in lat direction
-    num_long = 15 # number of images to take in long direction
-    spacing = 0.0025 # interval in degrees between images 0.0025 at zoom level 18
+    # spacing required for 19 zoom
+    spacing_long = 0.00171661376953125
+    spacing_lat = 0.0008583068847869413
     OFFSET = 0 # start of numbering of images
+
+    long_range = (10.5, 10.8)
+    lat_range = (59.88, 59.97)
+    coords_start = (59.88, 10.8)
+
+    num_lat = int((lat_range[1] - lat_range[0]) / spacing_lat)
+    num_long = int((long_range[1] - long_range[0]) / spacing_long)
+    
+    #num_lat = 3 # number of images to take in lat direction
+    #num_long = 3 # number of images to take in long direction
+
+    total_images = num_lat * num_long
+
+    input(f"Will fetch {total_images} images - press Enter to continue...")
+
     zoom = 19
     # make zero-array of coordinate pairs
     coords_grid = np.zeros((num_lat,num_long), dtype='f,f')
 
+    print(f"Starting at {coords_start}...")
+
     # cycle through each coordinate pair
     for i in range(num_lat):
         for j in range(num_long):
-            coords_grid[i,j] = (coords_start[0] + i * spacing, coords_start[1] + j * spacing)
+            coords_grid[i,j] = (coords_start[0] + i * spacing_lat, coords_start[1] + j * spacing_long)
     
     # make into 1D array of coordinate pairs
     coords_vec = coords_grid.flatten()
